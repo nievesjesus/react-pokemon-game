89a18e97d5701c1e8dacb45e1e062c80e1039bf9
JesusN <yo@nievesjesus.com>
2022-01-20 14:21:26 -0300

    new animation added

--- src/components/Status.jsx
+++ src/components/Status.jsx
@@ -1,13 +1,37 @@
+import Lottie from 'react-lottie';
+import * as animationData from '../assets/animations/sparks.json';
+
+
 const Status = ({ pokemon, catched }) => {
     const { name, sprites } = pokemon
-    console.log(pokemon);
+
+    const defaultOptions = {
+        loop: true,
+        autoplay: true, 
+        animationData: animationData,
+        rendererSettings: {
+          preserveAspectRatio: 'xMidYMid slice'
+        }
+      };
+
     return (
         <div className={`status-container ${catched ? "success" : "fail"}`}>
+            {catched ?
+            <div style={{ display: "flex", justifyContent: "center", alignItems: "center", position: "absolute", top: 0, left: 0, width: "100%", height: "100%" }}>
+                <Lottie options={defaultOptions}
+                        height={450}
+                        width={250}
+                        isStopped={false}
+                        isPaused={false}/>
+            </div> 
+            : 
+            null            
+            }           
             <div className="pokemon-preview">
                 <img src={sprites.other["home"]["front_default"]} alt="pokemon-preview" className="status-preview-img"/>
             </div>
             <div className="message">
-               {catched ? "Atrapaste a" : "Dejaste escapar a"} {name}
+               <span className="title-status">{catched ? "Atrapaste a" : "Dejaste escapar a"}</span> <br /> {name}
             </div>
         </div>
     )
